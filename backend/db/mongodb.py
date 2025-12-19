62be2b8c99392ac92f7788cce62d184da61ab086
liuyal <jerrylarryliu@gmail.com>
2025-12-18 16:44:09 -0800

    revert schema workaround

--- backend/db/mongodb.py
+++ backend/db/mongodb.py
@@ -21,12 +21,10 @@ from backend.app_def.app_def import (
     DB_COLLECTION_TCY
 )
 from backend.db.db import DatabaseClient, DBType, DBMode
-
-
-# from backend.models.projects import Project
-# from backend.models.test_cases import TestCase
-# from backend.models.test_cycles import TestCycle
-# from backend.models.test_executions import TestExecution
+from backend.models.projects import Project
+from backend.models.test_cases import TestCase
+from backend.models.test_cycles import TestCycle
+from backend.models.test_executions import TestExecution
 
 
 def pydantic_to_mongo_jsonschema(pydantic_schema: dict):
@@ -129,11 +127,16 @@ def pydantic_to_mongo_jsonschema(pydantic_schema: dict):
     }
 
 
+PROJECT_SCHEMA = pydantic_to_mongo_jsonschema(Project.model_json_schema())
+TEST_CASE_SCHEMA = pydantic_to_mongo_jsonschema(TestCase.model_json_schema())
+TEST_EXECUTION_SCHEMA = pydantic_to_mongo_jsonschema(TestExecution.model_json_schema())
+TEST_CYCLE_SCHEMA = pydantic_to_mongo_jsonschema(TestCycle.model_json_schema())
+
 DB_COLLECTIONS = [
-    DB_COLLECTION_PRJ,
-    DB_COLLECTION_TC,
-    DB_COLLECTION_TE,
-    DB_COLLECTION_TCY
+    (DB_COLLECTION_PRJ, PROJECT_SCHEMA),
+    (DB_COLLECTION_TC, TEST_CASE_SCHEMA),
+    (DB_COLLECTION_TE, TEST_EXECUTION_SCHEMA),
+    (DB_COLLECTION_TCY, TEST_CYCLE_SCHEMA)
 ]
 
 MONGODB_HOST = os.getenv("MONGODB_HOST", "localhost")
@@ -197,9 +200,10 @@ class MongoClient(DatabaseClient):
 
         # Initialize required collections
         collections = await self._db_client[self._db_name].list_collection_names()
-        for collection in DB_COLLECTIONS:
+        for collection, schema in DB_COLLECTIONS:
             if collection not in collections:
-                await self._db_client[self._db_name].create_collection(collection)
+                await self._db_client[self._db_name].create_collection(collection,
+                                                                       validator={"$jsonSchema": schema})
 
     async def create(self,
                      table: str,
